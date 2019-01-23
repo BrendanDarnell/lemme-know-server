fd4b3a1a5f8bb0e806bcb040de5d994a57563e6f
Brendan Darnell <brendan_darnell@hotmail.com>
2019-01-22 17:18:00 -0700

    Add client origin env variable for cors settings

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ exports.DATABASE_URL =
   process.env.DATABASE_URL || "mongodb://localhost/lemme-know";
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || "mongodb://localhost/test-lemme-know";
+exports.CLIENT_ORIGIN = 
+	process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.TWILIO_ID =  process.env.TWILIO_ID;
